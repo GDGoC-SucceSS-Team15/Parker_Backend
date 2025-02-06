e31ec4d1886a3a2db19c2629be6c841889ec4166
yina <yina20021018@gmail.com>
2025-02-06 16:10:05 +0900

    Feat: ParkingViolationController.java 추가

---
+++ parker/src/main/java/com/si9nal/parker/parkingvioation/controller/ParkingViolationController.java
@@ -0,0 +1,27 @@
+package com.si9nal.parker.parkingvioation.controller;
+
+import com.si9nal.parker.parkingvioation.dto.res.ParkingViolationResDto;
+import com.si9nal.parker.parkingvioation.service.ParkingViolationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+    @RequestMapping("/api/parkingViolation")
+    public class ParkingViolationController {
+        private final ParkingViolationService parkingViolationService;
+
+    public ParkingViolationController(ParkingViolationService parkingViolationService) {
+        this.parkingViolationService = parkingViolationService;
+    }
+
+    @GetMapping("/nearby")
+    public ResponseEntity<List<ParkingViolationResDto>> getParkingViolations(@RequestParam String sigunguName) {
+        List<ParkingViolationResDto> response = parkingViolationService.getBySigunguName(sigunguName);
+        return ResponseEntity.ok(response);
+    }
+}
