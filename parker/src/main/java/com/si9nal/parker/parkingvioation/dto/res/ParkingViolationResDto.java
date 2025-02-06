150b5db4f2937dc07bd4b59cd87301f7a53bc909
yina <yina20021018@gmail.com>
2025-02-06 16:12:22 +0900

    Feat: ParkingViolationResDto.java 추가

---
+++ parker/src/main/java/com/si9nal/parker/parkingvioation/dto/res/ParkingViolationResDto.java
@@ -0,0 +1,41 @@
+package com.si9nal.parker.parkingvioation.dto.res;
+
+import com.si9nal.parker.parkingvioation.domain.ParkingViolation;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalTime;
+
+@Getter
+@Builder
+public class ParkingViolationResDto {
+    private Long id;
+    private String sidoName;
+    private String sigunguName;
+    private String roadName;
+    private String detailedLocation;
+    private String managementPhoneNumber;
+    private LocalTime weekdayStartTime;
+    private LocalTime weekdayEndTime;
+    private LocalTime saturdayStartTime;
+    private LocalTime saturdayEndTime;
+    private LocalTime holidayStartTime;
+    private LocalTime holidayEndTime;
+
+    public static ParkingViolationResDto fromEntity(ParkingViolation parkingViolation) {
+        return ParkingViolationResDto.builder()
+                .id(parkingViolation.getId())
+                .sidoName(parkingViolation.getSidoName())
+                .sigunguName(parkingViolation.getSigunguName())
+                .roadName(parkingViolation.getRoadName())
+                .detailedLocation(parkingViolation.getDetailedLocation())
+                .managementPhoneNumber(parkingViolation.getManagementPhoneNumber())
+                .weekdayStartTime(parkingViolation.getWeekdayStartTime())
+                .weekdayEndTime(parkingViolation.getWeekdayEndTime())
+                .saturdayStartTime(parkingViolation.getSaturdayStartTime())
+                .saturdayEndTime(parkingViolation.getSaturdayEndTime())
+                .holidayStartTime(parkingViolation.getHolidayStartTime())
+                .holidayEndTime(parkingViolation.getHolidayEndTime())
+                .build();
+    }
+}
