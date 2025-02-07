c401afc450790dabceae69300c4cfc9fe3c19bba
yina <yina20021018@gmail.com>
2025-02-07 16:54:18 +0900

    Fix : 불법주정차 단속 위치 조회 @RequestMapping 통일

--- parker/src/main/java/com/si9nal/parker/parkingvioation/controller/ParkingViolationController.java
+++ parker/src/main/java/com/si9nal/parker/parkingvioation/controller/ParkingViolationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-    @RequestMapping("/api/parkingViolation")
+    @RequestMapping("/api/parking-violation")
     public class ParkingViolationController {
         private final ParkingViolationService parkingViolationService;
 
