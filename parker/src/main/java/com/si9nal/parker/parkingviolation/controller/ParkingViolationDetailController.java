7982e4fc461732631edb5092a6360c4f6335b66b
bigtr3 <tmddms1002@gmail.com>
2025-02-12 17:31:37 +0900

    Refactor : User / ParkingViolation API 스웨거 어노테이션 추가

--- parker/src/main/java/com/si9nal/parker/parkingviolation/controller/ParkingViolationDetailController.java
+++ parker/src/main/java/com/si9nal/parker/parkingviolation/controller/ParkingViolationDetailController.java
@@ -2,6 +2,8 @@ package com.si9nal.parker.parkingviolation.controller;
 
 import com.si9nal.parker.parkingviolation.dto.res.ParkingViolationDetailResponseDto;
 import com.si9nal.parker.parkingviolation.service.ParkingViolationDetailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/parking-violation")
+@Tag(name = "불법주정차 단속 구역 API", description = "주변 불법주정차 단속 구역 정보를 제공하는 API")
 public class ParkingViolationDetailController {
 
     private final ParkingViolationDetailService parkingViolationDetailService;
@@ -21,17 +24,29 @@ public class ParkingViolationDetailController {
     }
 
     @GetMapping
+    @Operation(
+            summary = "불법 주정차 단속 위치 정보 전체 조회",
+            description = "불법 주정차 단속 위치 정보를 모두 조회합니다."
+    )
     public ResponseEntity<ApiResponse<List<ParkingViolationDetailResponseDto>>> getAllParkingViolations() {
         List<ParkingViolationDetailResponseDto> allParkingViolations = parkingViolationDetailService.getAllParkingViolationDetails();
         return ResponseEntity.ok(ApiResponse.onSuccess(allParkingViolations));
     }
 
-    @GetMapping("/detailedLocation/{detailedLocation}")
+    @GetMapping("/detailed-location/{detailedLocation}")
+    @Operation(
+            summary = "불법 주정차 단속 위치 정보 상세위치별 상세 조회",
+            description = "불법 주정차 단속 위치의 detailedLocation을 기준으로 상세 정보를 조회합니다."
+    )
     public ResponseEntity<ApiResponse<ParkingViolationDetailResponseDto>> getParkingViolationDetail(@PathVariable String detailedLocation) {
         return ResponseEntity.ok(ApiResponse.onSuccess(parkingViolationDetailService.getParkingViolationDetailByDetailedLocation(detailedLocation)));
     }
 
     @GetMapping("/id/{id}")
+    @Operation(
+            summary = "불법 주정차 단속 위치 정보 ID별 상세 조회",
+            description = "불법 주정차 단속 위치의 ID를 기준으로 상세 정보를 조회합니다."
+    )
     public ResponseEntity<ApiResponse<ParkingViolationDetailResponseDto>> getParkingViolationDetailById(@PathVariable Long id) {
         return ResponseEntity.ok(ApiResponse.onSuccess(parkingViolationDetailService.getParkingViolationDetailById(id)));
     }
--- parker/src/main/java/com/si9nal/parker/user/controller/UserController.java
+++ parker/src/main/java/com/si9nal/parker/user/controller/UserController.java
@@ -6,6 +6,8 @@ import com.si9nal.parker.user.dto.req.UserSignupReqDto;
 import com.si9nal.parker.user.dto.res.TokenDto;
 import com.si9nal.parker.user.dto.res.UserInfoResDto;
 import com.si9nal.parker.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/user")
+@Tag(name = "유저 API", description = "회원 관련 기능을 제공하는 API")
 public class UserController {
 
     private final UserService userService;
@@ -22,22 +25,30 @@ public class UserController {
     }
 
     @PostMapping("/signup")
+    @Operation(summary = "회원가입 API",
+            description = "회원가입 API입니다.")
     public ResponseEntity<ApiResponse<UserInfoResDto>> signUp(@RequestBody UserSignupReqDto request) {
         return ResponseEntity.ok(ApiResponse.onSuccess(userService.signUp(request)));
     }
 
     @PostMapping("/login")
+    @Operation(summary = "로그인 API",
+            description = "로그인 API입니다. API 테스트시 해당 API를 실행한 후 발급되는 key로 로그인해주세요.")
     public ResponseEntity<ApiResponse<TokenDto>> login(@RequestBody UserLoginReqDto request) {
         return ResponseEntity.ok(ApiResponse.onSuccess(userService.login(request)));
     }
 
     @PostMapping("/logout")
+    @Operation(summary = "로그아웃 API",
+            description = "로그아웃 API입니다.")
     public ResponseEntity<ApiResponse<Void>> logout(Principal principal, HttpServletRequest request) {
         userService.logout(principal, request);
         return ResponseEntity.ok(ApiResponse.onSuccess(null));
     }
 
     @PostMapping("/delete-user")
+    @Operation(summary = "회원탈퇴 API",
+            description = "회원탈퇴 API입니다.")
     public ResponseEntity<ApiResponse<Void>> deleteUser(Principal principal, HttpServletRequest request) {
         userService.deleteUser(principal, request);
         return ResponseEntity.ok(ApiResponse.onSuccess(null));
