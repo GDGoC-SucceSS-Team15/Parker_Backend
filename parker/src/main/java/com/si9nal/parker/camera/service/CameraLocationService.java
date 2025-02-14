12291dd5c88d5192cb82b9c27c186223daa8b766
yina <yina20021018@gmail.com>
2025-02-14 11:36:06 +0900

    Feat : 메인 지도에서 단속 카메라만 위치 조회 api

---
+++ parker/src/main/java/com/si9nal/parker/camera/service/CameraLocationService.java
@@ -0,0 +1,55 @@
+package com.si9nal.parker.camera.service;
+
+import com.si9nal.parker.camera.repository.CameraLocationRepository;
+import com.si9nal.parker.global.common.apiPayload.code.status.ErrorStatus;
+import com.si9nal.parker.global.common.apiPayload.exception.GeneralException;
+import com.si9nal.parker.global.common.util.Direction;
+import com.si9nal.parker.global.common.util.GeometryUtil;
+import com.si9nal.parker.global.common.util.Location;
+import com.si9nal.parker.map.dto.response.CameraLocationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CameraLocationService {
+
+    private final CameraLocationRepository cameraLocationRepository;
+
+    public List<CameraLocationResponse> findNearbyCameras (Double latitude, Double longitude) {
+
+        validateCoordinates(latitude, longitude);
+
+        Location northEast = GeometryUtil.calculate(latitude, longitude, 2.0, Direction.NORTHEAST.getBearing());
+        Location southWest = GeometryUtil.calculate(latitude, longitude, 2.0, Direction.SOUTHWEST.getBearing());
+
+        Location camera_northEast = GeometryUtil.calculate(latitude, longitude, 0.5, Direction.NORTHEAST.getBearing());
+        Location camera_southWest = GeometryUtil.calculate(latitude, longitude, 0.5, Direction.SOUTHWEST.getBearing());
+
+        List<CameraLocationResponse> cameraLocations = cameraLocationRepository.findCameraLocationsWithinBounds(
+                        camera_southWest.getLatitude(), camera_northEast.getLatitude(),
+                        camera_southWest.getLongitude(), camera_northEast.getLongitude())
+                .stream()
+                .map(CameraLocationResponse::fromEntity)
+                .collect(Collectors.toList());
+
+        if(cameraLocations.isEmpty()) {
+            throw new GeneralException(ErrorStatus.CAMERA_LOCATION_NOT_FOUND);
+        }
+
+        return cameraLocations;
+    }
+
+    private void validateCoordinates(Double latitude, Double longitude) {
+        if (latitude == null || latitude < -90 || latitude > 90) {
+            throw new GeneralException(ErrorStatus.CAMERA_LOCATION_INVALID_LATITUDE);
+        }
+        if (longitude == null || longitude < -180 || longitude > 180) {
+            throw new GeneralException(ErrorStatus.CAMERA_LOCATION_INVALID_LONGITUDE);
+        }
+    }
+
+}
